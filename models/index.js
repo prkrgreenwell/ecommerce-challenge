43ca133473a032e0f421a516c94189321ccd70d3
prkrgreenwell <prkrgreenwell@gmail.com>
2022-12-20 23:43:40 -0700

    finished product

--- models/index.js
+++ models/index.js
@@ -17,19 +17,15 @@ Category.hasMany(Product, {
 });
 // Products belongToMany Tags (through ProductTag)
 Product.belongsToMany(Tag, {
-  through: {
-    model: ProductTag,
-    unique: false,
-  },
+  through: ProductTag,
   as: "tags_on_product",
+  foreignKey: "product_id",
 });
 // Tags belongToMany Products (through ProductTag)
 Tag.belongsToMany(Product, {
-  through: {
-    model: ProductTag,
-    unique: false,
-  },
+  through: ProductTag,
   as: "products_with_tag",
+  foreignKey: "tag_id",
 });
 
 module.exports = {
