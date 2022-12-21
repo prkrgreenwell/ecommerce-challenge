f6e17546c5283602c57adfb0c9b0ddc6a0c17764
prkrgreenwell <prkrgreenwell@gmail.com>
2022-12-20 23:04:02 -0700

    completed routes, need to fix ProductTag stuff

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -14,7 +14,7 @@ router.get("/", async (req, res) => {
     });
     res.status(200).json(categoryData);
   } catch (err) {
-    res.status(500).json * err;
+    res.status(500).json(err);
   }
 });
 
